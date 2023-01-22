4289016824c9ae6aa461e1bfcbfa94b08a6d28b5
master <irmitcod>
2023-01-22 05:10:54 +0330

    Add customer module

---
+++ customers/module.go
@@ -0,0 +1,34 @@
+package customers
+
+import (
+	"context"
+
+	"eda-shops/customers/internal/application"
+	"eda-shops/customers/internal/grpc"
+	"eda-shops/customers/internal/logging"
+	"eda-shops/customers/internal/postgres"
+	"eda-shops/customers/internal/rest"
+	"eda-shops/internal/monolith"
+)
+
+type Module struct{}
+
+func (m Module) Startup(ctx context.Context, mono monolith.Monolith) error {
+	customers := postgres.NewCustomerRepository("customers.customers", mono.DB())
+
+	var app application.App
+	app = application.New(customers)
+	app = logging.LogApplicationAccess(app, mono.Logger())
+
+	if err := grpc.RegisterServer(app, mono.RPC()); err != nil {
+		return err
+	}
+	if err := rest.RegisterGateway(ctx, mono.Mux(), mono.Config().Rpc.Address()); err != nil {
+		return err
+	}
+	if err := rest.RegisterSwagger(mono.Mux()); err != nil {
+		return err
+	}
+
+	return nil
+}

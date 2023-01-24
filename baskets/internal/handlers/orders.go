cca0469f767dd82f87245e597272e5f365e889d4
master <irmitcod>
2023-01-24 06:29:33 +0330

    Add Customer Events

--- baskets/internal/handlers/orders.go
+++ baskets/internal/handlers/orders.go
@@ -1,11 +1,10 @@
 package handlers
 
 import (
-	"eda-shops/baskets/internal/application"
 	"eda-shops/baskets/internal/domain"
 	"eda-shops/internal/ddd"
 )
 
-func RegisterOrderHandlers(orderHandlers application.DomainEventHandlers, domainSubscriber ddd.EventSubscriber) {
-	domainSubscriber.Subscribe(domain.BasketCheckedOut{}, orderHandlers.OnBasketCheckedOut)
+func RegisterOrderHandlers(orderHandlers ddd.EventHandler[ddd.AggregateEvent], domainSubscriber ddd.EventSubscriber[ddd.AggregateEvent]) {
+	domainSubscriber.Subscribe(domain.BasketCheckedOutEvent, orderHandlers)
 }
--- baskets/module.go
+++ baskets/module.go
@@ -4,21 +4,35 @@ import (
 	"context"
 
 	"eda-shops/baskets/internal/application"
+	"eda-shops/baskets/internal/domain"
 	"eda-shops/baskets/internal/grpc"
 	"eda-shops/baskets/internal/handlers"
 	"eda-shops/baskets/internal/logging"
-	"eda-shops/baskets/internal/postgres"
 	"eda-shops/baskets/internal/rest"
 	"eda-shops/internal/ddd"
+	"eda-shops/internal/es"
 	"eda-shops/internal/monolith"
+	pg "eda-shops/internal/postgres"
+	"eda-shops/internal/registry"
+	"eda-shops/internal/registry/serdes"
 )
 
 type Module struct{}
 
 func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) (err error) {
 	// setup Driven adapters
-	domainDispatcher := ddd.NewEventDispatcher()
-	baskets := postgres.NewBasketRepository("baskets.baskets", mono.DB())
+	reg := registry.New()
+	err = registrations(reg)
+	if err != nil {
+		return err
+	}
+	domainDispatcher := ddd.NewEventDispatcher[ddd.AggregateEvent]()
+	aggregateStore := es.AggregateStoreWithMiddleware(
+		pg.NewEventStore("baskets.events", mono.DB(), reg),
+		es.NewEventPublisher(domainDispatcher),
+		pg.NewSnapshotStore("baskets.snapshots", mono.DB(), reg),
+	)
+	baskets := es.NewAggregateRepository[*domain.Basket](domain.BasketAggregate, reg, aggregateStore)
 	conn, err := grpc.Dial(ctx, mono.Config().Rpc.Address())
 	if err != nil {
 		return err
@@ -29,12 +43,12 @@ func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) (err error
 
 	// setup application
 	app := logging.LogApplicationAccess(
-		application.New(baskets, stores, products, orders, domainDispatcher),
+		application.New(baskets, stores, products, orders),
 		mono.Logger(),
 	)
-	orderHandlers := logging.LogDomainEventHandlerAccess(
+	orderHandlers := logging.LogEventHandlerAccess[ddd.AggregateEvent](
 		application.NewOrderHandlers(orders),
-		mono.Logger(),
+		"Order", mono.Logger(),
 	)
 
 	// setup Driver adapters
@@ -50,4 +64,39 @@ func (m *Module) Startup(ctx context.Context, mono monolith.Monolith) (err error
 	handlers.RegisterOrderHandlers(orderHandlers, domainDispatcher)
 
 	return
+}
+
+func registrations(reg registry.Registry) error {
+	serde := serdes.NewJsonSerde(reg)
+
+	// Basket
+	if err := serde.Register(domain.Basket{}, func(v interface{}) error {
+		basket := v.(*domain.Basket)
+		basket.Items = make(map[string]domain.Item)
+		return nil
+	}); err != nil {
+		return err
+	}
+	// basket events
+	if err := serde.Register(domain.BasketStarted{}); err != nil {
+		return err
+	}
+	if err := serde.Register(domain.BasketCanceled{}); err != nil {
+		return err
+	}
+	if err := serde.Register(domain.BasketCheckedOut{}); err != nil {
+		return err
+	}
+	if err := serde.Register(domain.BasketItemAdded{}); err != nil {
+		return err
+	}
+	if err := serde.Register(domain.BasketItemRemoved{}); err != nil {
+		return err
+	}
+	// basket snapshots
+	if err := serde.RegisterKey(domain.BasketV1{}.SnapshotName(), domain.BasketV1{}); err != nil {
+		return err
+	}
+
+	return nil
 }
